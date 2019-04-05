4df45afe48b134755ac169861f997b8f87c2e8a0
Montgomery-Whalen <37054753+Montgomery-Whalen@users.noreply.github.com>
2019-04-04 17:54:35 -0700

    Gets dimension of puzzle from user
    got pygame set up on my computer too but that's for me!

---
+++ WitnessSolver.py
@@ -0,0 +1,11 @@
+#This solves puzzles from the game the witness
+import pygame
+
+def main():
+    getDimensions()#ask for dimensions of puzzle()
+        
+def getDimensions():
+    w = input("What is the width of the puzzle?")
+    h = input("What is the height of the puzzle?")
+
+main()
